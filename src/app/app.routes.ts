5ffc6bc2a2d5ca88b356df78401d22926ab861f9
joseGiraudo <josegiraudo7@gmail.com>
2025-04-17 10:15:55 -0300

    add bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MemberFormComponent } from './components/member-form/member-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'member-form', component: MemberFormComponent },
+];
