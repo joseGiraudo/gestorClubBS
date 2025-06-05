61b54def79e8d1e1648d4c48500f47b1b8b08a3a
joseGiraudo <josegiraudo7@gmail.com>
2025-06-05 19:58:02 -0300

    news form toast

--- src/app/components/news/news-form/news-form.component.ts
+++ src/app/components/news/news-form/news-form.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NewsService } from '../../../services/news.service';
 import { CreateNews, News } from '../../../models/news';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-news-form',
@@ -9,10 +11,14 @@ import { CreateNews, News } from '../../../models/news';
   templateUrl: './news-form.component.html',
   styleUrl: './news-form.component.css'
 })
-export class NewsFormComponent {
+export class NewsFormComponent implements OnInit {
 
   newsForm: FormGroup;
   selectedFile: File | null = null;
+
+  // toast  
+  private toastElement: any;
+  private toast: any;
 
   private newsService = inject(NewsService);
 
@@ -23,6 +29,17 @@ export class NewsFormComponent {
       content: ['', Validators.required],
       date: ['', Validators.required]
     });
+  }
+
+  ngOnInit(): void {
+    // Inicializar el toast
+    this.toastElement = document.getElementById('responseToast');
+    if (this.toastElement) {
+      this.toast = new bootstrap.Toast(this.toastElement, {
+        autohide: true,
+        delay: 4000 // 4 segundos
+      });
+    }
   }
 
   onFileChange(event: any) {
@@ -45,9 +62,53 @@ export class NewsFormComponent {
 
     if (this.selectedFile) {
       this.newsService.createNews(news, this.selectedFile).subscribe({
-        next: (res) => console.log('Éxito:', res),
-        error: (err) => console.error('Error:', err)
+        next: (res) => {
+          console.log('Éxito:', res)
+          this.showToast("Noticia cargada con éxito", 'success')
+        },
+        error: (err) => {
+          console.error('Error:', err);
+          console.log(err.error.message)
+          this.showToast("Error al cargar la noticia", 'error')
+        }
       });
     }
   }
+
+
+
+  // Método para mostrar toast
+  private showToast(message: string, type: 'success' | 'error'): void {
+    const toastBody = document.getElementById('toastBody');
+    const toastIcon = document.getElementById('toastIcon');
+    const toastContainer = document.getElementById('responseToast');
+    
+    if (toastBody && toastIcon && toastContainer) {
+      // Configurar el mensaje
+      toastBody.textContent = message;
+      
+      // Configurar el estilo según el tipo
+      if (type === 'success') {
+        toastContainer.className = 'toast align-items-center text-bg-success border-0';
+        toastIcon.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.061L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+          </svg>
+        `;
+      } else {
+        toastContainer.className = 'toast align-items-center text-bg-danger border-0';
+        toastIcon.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+          </svg>
+        `;
+      }
+      
+      // Mostrar el toast
+      this.toast.show();
+    }
+  }
+
+
+
 }
