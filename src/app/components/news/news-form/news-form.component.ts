ff7f94ee055f8b99cbb0eab7c235d8f99a5dedcb
joseGiraudo <josegiraudo7@gmail.com>
2025-06-20 15:27:12 -0300

    image file selected for news

--- src/app/components/news/news-form/news-form.component.ts
+++ src/app/components/news/news-form/news-form.component.ts
@@ -17,6 +17,8 @@ export class NewsFormComponent implements OnInit {
   newsForm: FormGroup;
   selectedFile: File | null = null;
 
+  allowedImageTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp', 'image/gif'];
+
   // toast  
   private toastElement: any;
   private toast: any;
@@ -55,8 +57,9 @@ export class NewsFormComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.newsForm.invalid || !this.selectedFile) return;
+    if (this.newsForm.invalid) return;
 
+    console.log(this.selectedFile);
 
     const news: CreateNews = {
       title: this.newsForm.get('title')?.value,
@@ -66,24 +69,30 @@ export class NewsFormComponent implements OnInit {
     };
 
     if (this.selectedFile) {
-      this.newsService.createNews(news, this.selectedFile).subscribe({
-        next: (res) => {
-          console.log('Éxito:', res)
-          this.showToast("Noticia cargada con éxito", 'success')
-        },
-        error: (err) => {
-          console.error('Error:', err);
-          console.log(err.error.message)
-          this.showToast("Error al cargar la noticia", 'error')
-        }
-      });
+      if(this.selectedFile.type && this.allowedImageTypes.includes(this.selectedFile.type) ) {
+        
+        this.showToast('Cargando noticia', 'loading');
+        this.newsService.createNews(news, this.selectedFile).subscribe({
+          next: (res) => {
+            console.log('Éxito:', res)
+            this.showToast("Noticia cargada con éxito", 'success')
+          },
+          error: (err) => {
+            console.error('Error:', err);
+            console.log(err.error.message)
+            this.showToast("Error al cargar la noticia", 'error')
+          }
+        });
+      } else {
+        this.showToast("Debes seleccionar un archivo de imagen (.jpg .jpeg .png)", 'error')
+      }
     }
   }
 
 
 
   // Método para mostrar toast
-  private showToast(message: string, type: 'success' | 'error'): void {
+  private showToast(message: string, type: 'success' | 'error' | 'loading'): void {
     const toastBody = document.getElementById('toastBody');
     const toastIcon = document.getElementById('toastIcon');
     const toastContainer = document.getElementById('responseToast');
@@ -93,20 +102,38 @@ export class NewsFormComponent implements OnInit {
       toastBody.textContent = message;
       
       // Configurar el estilo según el tipo
-      if (type === 'success') {
-        toastContainer.className = 'toast align-items-center text-bg-success border-0';
-        toastIcon.innerHTML = `
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.061L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-          </svg>
-        `;
-      } else {
-        toastContainer.className = 'toast align-items-center text-bg-danger border-0';
-        toastIcon.innerHTML = `
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
-            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-          </svg>
-        `;
+      switch (type) {
+        case 'success':
+          toastContainer.className = 'toast align-items-center text-bg-success border-0';
+          toastIcon.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.061L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </svg>
+          `;
+          break;
+
+        case 'error':
+          toastContainer.className = 'toast align-items-center text-bg-danger border-0';
+          toastIcon.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+              <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+          `;
+          break;
+
+        case 'loading':
+          toastContainer.className = 'toast align-items-center text-bg-secondary border-0';
+          toastIcon.innerHTML = `
+            <div class="spinner-border spinner-border-sm text-light" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          `;
+          break;
+
+        default:
+          toastContainer.className = 'toast align-items-center text-bg-info border-0';
+          toastIcon.innerHTML = '';
+          break;
       }
       
       // Mostrar el toast
