0dccd0cd53811285163e51722f746c0c29e1033a
joseGiraudo <josegiraudo7@gmail.com>
2026-04-22 18:06:05 -0300

    fix url enviroment

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend.onrender.com',
-  mpUrl: 'https://tu-backend.onrender.com/api/v1/mp'
+  apiUrl: 'https://gestorclub-be.onrender.com',
+  mpUrl: 'https://gestorclub-be.onrender.com/api/v1/mp'
 };
