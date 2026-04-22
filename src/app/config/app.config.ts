32afb44b5eddc164006d71d88298fe08d5b30b53
joseGiraudo <josegiraudo7@gmail.com>
2026-04-22 18:00:26 -0300

    fix url enviroment

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,4 +1,6 @@
+import { environment } from "../../enviroments/enviroment.prod";
+
 export const APP_CONFIG = {
-  apiUrl: (globalThis as any)?.process?.env?.['API_URL'] || 'http://localhost:8080',
-  mpUrl: (globalThis as any)?.process?.env?.['MP_URL'] || 'http://localhost:8080/api/v1/mp'
+  apiUrl: environment.apiUrl,
+  mpUrl: environment.mpUrl
 };
---
+++ src/enviroments/enviroment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://tu-backend.onrender.com',
+  mpUrl: 'https://tu-backend.onrender.com/api/v1/mp'
+};
